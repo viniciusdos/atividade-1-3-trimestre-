a3f2d4e6f5fc2588ef91681e4e4c30c0d9322484
vinicius <142812151+viniciusdos@users.noreply.github.com>
2024-10-04 11:33:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'qual é o melhor jogador do mundo?',
+    'Cristiano ronaldo (CR7)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da russia?',
+    'A capital da russia é moscou'
+)
+
+criaCartao(
+    'animal',
+    'qual maior mamifero do mundo?',
+    'maior mamifero do mundo é baleia-azul'
+)
+
+criaCartao(
+    'filme',
+    'qual o ator principal do filme velozes e furiosos?',
+    'é o Vin Diesel'
+)
